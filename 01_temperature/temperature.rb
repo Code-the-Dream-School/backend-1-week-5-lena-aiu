a222b0501a5c0188b6f16b0d2c07ca005068ba64
Lena Aiupova <e.ayupova@gmail.com>
2020-09-27 00:03:23 -0400

    Question about Simon says file

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+def ftoc(fahrenheit)
+    celcium = (fahrenheit.to_i - 32) * 5.0 / 9.0
+    celcium.round
+end    
+
+def ctof(celcium)
+    fahrenheit = (celcium.to_i * 9.0 / 5.0) + 32
+    fahrenheit.round(1)
+end    
