148f772396ea3a50bd2506caed9f0339a3214ed0
Lena Aiupova <e.ayupova@gmail.com>
2020-09-27 21:22:20 -0400

    Please review the assignment

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,7 +1,33 @@
 #write your code here
-def Simon says
 
-    def echo(hello)
-        "should echo hello #{hello}"
-    end     
-end   
+def echo(greeting)
+        greeting
+end
+
+def shout(greeting)
+    greeting.upcase
+end
+
+def repeat(greeting, c=2)
+        [greeting] * c * " "
+end
+
+def start_of_word(greeting, num)
+    greeting[0..(num-1)]
+end    
+
+def first_word(words)
+    words.split.first
+end
+
+def titleize(words)
+    each_word = words.split.map do |word|
+      if %w(the and over).include?(word)
+        word
+      else
+        word.capitalize!
+      end
+    end
+    #each_word.first.capitalize!
+    each_word.join(" ")
+end  
